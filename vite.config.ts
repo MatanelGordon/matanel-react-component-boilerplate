410b6f87f2797108ea59c5183d9ef6603904cf49
Matanel Mac <mmgordon.matanel@gmail.com>
2022-11-11 00:25:08 +0200

    eslint vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import {defineConfig} from "vitest/config";
 
 export default defineConfig({
-    test: {
-        include: ["**/*.test.ts", "**/*.test.tsx", "**/*.spec.ts", "**/*.spec.tsx"],
-        exclude: ["node_modules/", "dist/"]
-    }
+	test: {
+		include: ["**/*.test.ts", "**/*.test.tsx", "**/*.spec.ts", "**/*.spec.tsx"],
+		exclude: ["node_modules/", "dist/"]
+	}
 });
