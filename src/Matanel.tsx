8071f83697713c2874e02f6564c4851ddf5b7027
Matanel Mac <mmgordon.matanel@gmail.com>
2022-11-18 22:57:30 +0200

    added prettier

--- src/Matanel.tsx
+++ src/Matanel.tsx
@@ -1,15 +1,16 @@
-import {FC, useEffect, useState} from "react";
+import { FC, useEffect, useState } from 'react';
 
 export const Matanel: FC = () => {
 	const [count, setCount] = useState(0);
 
 	useEffect(() => {
-		// eslint-disable-next-line no-console
-		console.log('count changed: ', count)
-	}, [count])
+		console.log('count changed: ', count);
+	}, [count]);
 
-	return (<>
-		<button onClick={() => setCount(i => i + 1)}>count</button>
-		{!!count && <span>{count}</span>}
-	</>)
-}
+	return (
+		<>
+			<button onClick={() => setCount(i => i + 1)}>count</button>
+			{!!count && <span>{count}</span>}
+		</>
+	);
+};
--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-	extends: "stylelint-config-recommended",
+	extends: ["stylelint-prettier/recommended", "stylelint-config-recommended"],
+	"plugins": ["stylelint-prettier"],
 	rules: {
-		"selector-class-pattern": /^([a-z][a-z0-9]*)(-[a-z0-9]+)*$/
+		"selector-class-pattern": /^([a-z][a-z0-9]*)(-[a-z0-9]+)*$/,
+		"prettier/prettier": true
 	}
 }
