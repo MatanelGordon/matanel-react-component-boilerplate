4b843b83c1e160f1527ec8e62f64373c4e8999c4
Matanel Mac <mmgordon.matanel@gmail.com>
2022-11-19 11:24:30 +0200

    removed prettier plugins for eslint and stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,5 @@
 module.exports = {
 	extends: ["stylelint-prettier/recommended", "stylelint-config-recommended"],
-	"plugins": ["stylelint-prettier"],
 	rules: {
 		"selector-class-pattern": /^([a-z][a-z0-9]*)(-[a-z0-9]+)*$/,
 		"prettier/prettier": true
