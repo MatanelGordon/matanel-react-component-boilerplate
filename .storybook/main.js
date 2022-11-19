4968726d41884ed7bcbc364d472efa9ac5d1b841
Matanel Mac <mmgordon.matanel@gmail.com>
2022-11-19 15:30:33 +0200

    removed react docgen

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,8 @@
 /** @type {import('@storybook/react/types').StorybookConfig} */
 module.exports = {
-	typescript: {},
+	typescript: {
+		reactDocgen: false
+	},
 	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
 	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
 	framework: '@storybook/react',
