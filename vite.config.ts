009f06538118db6c901e84e65238b3fec4d45075
Matanel Mac <mmgordon.matanel@gmail.com>
2023-04-27 22:43:42 +0300

    removed globals

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
-import {defineConfig} from "vitest/config";
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 	plugins: [react()],
 	test: {
-		globals: true,
 		environment: 'jsdom',
-		include: ["**/*.@(test|spec).@(ts|tsx)"],
-		exclude: ["node_modules", "dist"]
-	}
+		include: ['**/*.@(test|spec).@(ts|tsx)'],
+		exclude: ['node_modules', 'dist'],
+	},
 });
